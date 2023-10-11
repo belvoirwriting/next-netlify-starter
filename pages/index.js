49ce71996fe42aa38fc765835e8d07403e70b1b6
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2023-10-11 20:36:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Vale of Belvoir Writing Group" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          second Tuesday of the month
         </p>
       </main>
 
